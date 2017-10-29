7ab186122c668728bda4642461dc472436df05b9
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2017-10-29 07:43:28 +0700

    fixing post data handler

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import cgi
 import time
-from os import path, curdir, sep
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import middleware
+from os import path, curdir, sep
 from config import SERVER_NAME, HOST, PORT
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 middlewareHandle = middleware.security.Authorize()
 
@@ -43,9 +44,16 @@ class ServerHandler(BaseHTTPRequestHandler):
 				return
 
 	def do_POST(self):
-		contentLen = int(self.headers.getheader('content-length', 0))
-		postBody = self.rfile.read(contentLen)
-		print "Checking our data %s" % postBody
+		ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+		if ctype == 'multipart/form-data':
+			postvars = cgi.parse_multipart(self.rfile, pdict)
+		elif ctype == 'application/x-www-form-urlencoded':
+			length = int(self.headers.getheader('content-length'))
+			postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+		else:
+			postvars = {}
+		print "Checking our data %s" % ctype
+		print "Data are %s" % postvars
 
 if __name__ == '__main__':
 	server = HTTPServer((HOST, PORT), ServerHandler)
