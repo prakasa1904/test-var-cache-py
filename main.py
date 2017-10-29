c1336ca13749345a9c2c654cfbd360bdc7ced28e
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2017-10-29 08:31:20 +0700

    fixing post handler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cgi
 import time
+import json
 import middleware
 from os import path, curdir, sep
 from config import SERVER_NAME, HOST, PORT
@@ -44,21 +45,23 @@ class ServerHandler(BaseHTTPRequestHandler):
 				return
 
 	def do_POST(self):
+		self.server_version = SERVER_NAME
 		ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 		if ctype == 'multipart/form-data':
-			postvars = cgi.parse_multipart(self.rfile, pdict)
+			postVars = cgi.parse_multipart(self.rfile, pdict)
 		elif ctype == 'application/x-www-form-urlencoded':
 			length = int(self.headers.getheader('content-length'))
-			postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+			postVars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
 		elif ctype == 'application/json':
 			length = int(self.headers.getheader('content-length'))
-			postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+			postVars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
 		else:
-			postvars = {}
-		print "Checking our data %s" % ctype
-		print "Data are %s" % postvars
-		print "Data type is %s" % type(postvars)
-		print "Data name is %s" % postvars['name'][0]
+			postVars = {}
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		self.wfile.write(json.dumps(postVars))
+		return
 
 if __name__ == '__main__':
 	server = HTTPServer((HOST, PORT), ServerHandler)
